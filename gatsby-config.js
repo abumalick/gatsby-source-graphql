02535ecb7ef051a2a747f47f3055706842778000
abumalick <abdullah.hilson@gmail.com>
2018-10-04 17:29:54 +0200

    simplify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,21 @@
+const axios = require('axios')
+
 module.exports = {
   plugins: [
-    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-graphql`,
       options: {
-        fieldName: `cms`,
+        fieldName: `fugoki`,
         url: `https://api-euwest.graphcms.com/v1/cjjr1at6d0xb801c3scjrm0l0/master`,
-        typeName: `GraphCMS`,
-        refetchInterval: 60,
+        typeName: `FUGOKI`,
+        // refetchInterval: 60,
+        createSchema: async () => {
+          // const sdl = fs.readFileSync(`${__dirname}/schema.sdl`).toString()
+          const request = await axios('http://perdu.com')
+          const sdl = request.data
+          console.log(request.data)
+          return buildSchema(sdl)
+        },
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
-import { graphql } from "gatsby"
+// import { graphql } from "gatsby"
 import React from "react"
-import { makeBlogPath } from "../utils"
-import dateformat from "dateformat"
 
 export default ({ data }) => (
   <div>
@@ -11,24 +9,17 @@ export default ({ data }) => (
         Using gatsby-source-graphql
       </a>
     </p>
-    {data.cms.blogPosts.map((blog, i) => (
-      <a key={i} href={makeBlogPath(blog)}>
-        <h2>
-          {dateformat(blog.createdAt, `fullDate`)} - {blog.title}
-        </h2>
-      </a>
-    ))}
   </div>
 )
 
-export const query = graphql`
-  query {
-    cms {
-      blogPosts(where: { status: PUBLISHED }, orderBy: createdAt_DESC) {
-        title
-        createdAt
-        slug
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     cms {
+//       blogPosts(where: { status: PUBLISHED }, orderBy: createdAt_DESC) {
+//         title
+//         createdAt
+//         slug
+//       }
+//     }
+//   }
+// `
